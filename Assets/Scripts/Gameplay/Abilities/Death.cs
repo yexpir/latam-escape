7afb2b4134f2c932d6da888ded321ab51f6c7632
Salvador Novo <salvanovodev@gmail.com>
2025-05-30 23:13:44 -0300

    I re-enabled death

--- Assets/Scripts/Gameplay/Abilities/Death.cs
+++ Assets/Scripts/Gameplay/Abilities/Death.cs
@@ -6,15 +6,6 @@ namespace Gameplay.Abilities
     public class Death : Ability
     {
         public override void Init() => SetRaiser(PlayerController.OnDie);
-        protected override void Execute() => foo();//GameManager.ResetCurrentScene();
-
-        void foo()
-        {
-        }
-
-        void OnEnable()
-        {
-            
-        }
+        protected override void Execute() => GameManager.ResetCurrentScene();
     }
 }
